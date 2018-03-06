da33f866b94e5604f67faf1cecccb73de4699ba7
coockieMonster88 <pajura_pawel@o2.pl>
2018-03-06 11:23:10 +0100

    zadanie 14.4

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,5 +1,6 @@
-package com.kodilla.tasks;
+package com.crud.tasks;
 
+import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TasksApplication {
 
 	public static void main(String[] args) {
+		TaskDto taskDto = new TaskDto((long)1,"Test title", "I want to be a coder");
 		SpringApplication.run(TasksApplication.class, args);
 	}
 }
--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kodilla.tasks;
+package com.crud.tasks;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
