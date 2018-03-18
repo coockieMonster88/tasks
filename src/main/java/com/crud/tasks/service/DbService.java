6f0cf0443f3ee4414ab32d3dae8e4c61d7eb9ba7
coockieMonster88 <pajura_pawel@o2.pl>
2018-03-18 19:02:53 +0100

    Fix

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,6 +1,19 @@
 package com.crud.tasks.service;
 
-public class DbService {
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.xml.ws.ServiceMode;
+import java.util.List;
+@Service
+public class DbService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTask(){
+        return taskRepository.findAll();
+    }
 
 }
