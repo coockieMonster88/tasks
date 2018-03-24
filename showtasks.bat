f251f3f01c75261bb0b78c5ee0fd6ab4d81d59fa
coockieMonster88 <pajura_pawel@o2.pl>
2018-03-24 20:58:46 +0100

    Zadanie 17.4

---
+++ showtasks.bat
@@ -0,0 +1,14 @@
+   ping 192.0.2.2 -n 1 -w 10000 > nul
+   explorer "http://localhost:8080/crud/v1/task/getTasks"
+   if "%ERRORLEVEL%" == "0" goto rename
+   echo.
+   echo firefox has errors - breaking work
+   goto fail
+
+   :fail
+   echo.
+   echo There were errors
+
+   :end
+   echo.
+   echo Work is finished.
